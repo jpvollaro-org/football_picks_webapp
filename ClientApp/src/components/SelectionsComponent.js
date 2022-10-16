3ed580aa0cae1da96286f0d8606fd058bb6bb2c3
Vollaro, John <jpvollaro@gmail.com>
2022-10-16 14:53:33 -0400

    Update SelectionsComponent.js
    Enhancement to allow players to make selections

--- ClientApp/src/components/SelectionsComponent.js
+++ ClientApp/src/components/SelectionsComponent.js
@@ -3,11 +3,11 @@ import useInput from '../hooks/use-input';
 import useHttps from '../hooks/use-https';
 import SelectionOptionComponent from "./SelectionOptionComponent";
 import GameOfWeekComponent from './GameOfWeekComponent';
-import TableComponent from './TableComponent';
 
-const SelectionsComponent = (props) =>
-{
-	let returnValue = { playerKey: "", thursdayWinner: "", sundayWinner: "", mondayWinner: "", awayTeamScore: 0, homeTeamScore: 0 };
+const SelectionsComponent = (props) => {
+	let playerSelectionArray = [{}, {}, {}, {}, {} ]
+
+	const [responseMessage, setResponseMessage] = useState("");
 
 	const [data, setData] = useState([]);
 	const transformData = ((incomingData) => {
@@ -22,32 +22,47 @@ const SelectionsComponent = (props) =>
 	}, []);
 
 	const resultHandler = (event) => {
-		console.log(event);
-		if (event.tabIndex === 1)
-			returnValue.thursdayWinner = event.label;
-		else if (event.tabIndex === 2)
-			returnValue.sundayWinner = event.label;
-		else if (event.tabIndex === 3)
-			returnValue.mondayWinner = event.label;
+		if (event.label === event.lookupKey) {
+			playerSelectionArray[event.tabIndex].homeTeam = event.lookupKey;
+			playerSelectionArray[event.tabIndex].homeScore = 100;
+			playerSelectionArray[event.tabIndex].awayScore = 0;
+			playerSelectionArray[event.tabIndex].awayTeam = event.opp;
+		}
+		else {
+			playerSelectionArray[event.tabIndex].homeTeam = event.lookupKey;
+			playerSelectionArray[event.tabIndex].homeScore = 0;
+			playerSelectionArray[event.tabIndex].awayScore = 100;
+			playerSelectionArray[event.tabIndex].awayTeam = event.label;
+		}
 	}
 
-	const setScore = (value) => {
-		returnValue.awayTeamScore = value;
+	const setScore = (e) => {
+		if (e.target.title === "home score") {
+			const v = playerSelectionArray[e.target.tabIndex].homeScore;
+			playerSelectionArray[e.target.tabIndex].homeScore = e.target.validity.valid ? e.target.value : v
+			playerSelectionArray[e.target.tabIndex].homeTeam = e.target.alt;
+		}
+		else {
+			const v = playerSelectionArray[e.target.tabIndex].awayScore;
+			playerSelectionArray[e.target.tabIndex].awayScore = e.target.validity.valid ? e.target.value : v
+			playerSelectionArray[e.target.tabIndex].awayTeam = e.target.alt;
+		}
 	}
 
 	const {
 		value: enteredPlayerKey,
-		isValid: playerKeyIsValid,
-		hasError: playerKeyHasError,
 		valueChangeHandler: playerKeyChangeHandler,
 	} = useInput(value => value.trim() !== '');
 
-	const UpdateSpreadsheetWithFileHandler = (event) => {
+	const selectionResponseHandler = (resp) => {
+		setResponseMessage(resp.responseMessage);
+	}
+
+	const SubmitButtonHandler = (event) => {
 		event.preventDefault();
-			//var urlString = "api/RegressionClaims/UpdateSpreadsheetWithFile?filepath=" + enteredFilepath
-			//	+ "&filename=" + enteredFilename
-			//	+ "&allowDuplicates=" + allowDuplicates;
-			//sendToClaimsController({ url: urlString, method: 'Put' }, props.onReturnFunction);
+		const headersValues = { 'content-type': 'application/json', 'playerKey': enteredPlayerKey };
+		var urlString = "api/ReactProgram/SendPlayerWeeklySelections";
+		sendToController({ url: urlString, method: 'Put', headers: headersValues, body: playerSelectionArray }, selectionResponseHandler);
 	};
 
 	if (isLoading) {
@@ -79,18 +94,19 @@ const SelectionsComponent = (props) =>
 				</div>
 			</div>
 			<br/>
-			<SelectionOptionComponent label='Thursday:' gameScore={data[0]} changeHandler={resultHandler} tabIndex={1} />
-			<SelectionOptionComponent label='Sunday:' gameScore={data[1]} changeHandler={resultHandler} tabIndex={2} />
-			<SelectionOptionComponent label='Monday:' gameScore={data[2]} changeHandler={resultHandler} tabIndex={3} />
+			<SelectionOptionComponent label='Thursday:' gameScore={data[0]} changeHandler={resultHandler} tabIndex={0} />
+			<SelectionOptionComponent label='Sunday:' gameScore={data[1]} changeHandler={resultHandler} tabIndex={1} />
+			<SelectionOptionComponent label='Monday:' gameScore={data[2]} changeHandler={resultHandler} tabIndex={2} />
 			<br />
-			<GameOfWeekComponent gameScore={data[3]} side="away" teamScore={returnValue.awayScore} SetScore={setScore} />
-			<GameOfWeekComponent gameScore={data[3]} side="home" teamScore={returnValue.homeScore} SetScore={setScore} />
+			<GameOfWeekComponent gameScore={data[3]} side="away score" teamScore={playerSelectionArray[3].awayScore} SetScore={setScore} tabIndex={3} />
+			<GameOfWeekComponent gameScore={data[3]} side="home score" teamScore={playerSelectionArray[3].homeScore} SetScore={setScore} tabIndex={3} />
 			<div className="container">
 				<br />
 				<div className="row">
 					<div className="col-sm-2" />
 					<div className="col-sm-4">
-						<button onClick={UpdateSpreadsheetWithFileHandler}>Submit</button>
+						<button onClick={SubmitButtonHandler}>Submit</button>
+						<p>{responseMessage}</p>
 					</div>
 				</div>
 			</div>
