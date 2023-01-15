8ef8561e0129f1bd08a965c60edc13b23800f2e2
Vollaro, John <john.vollaro@optum.com>
2023-01-15 09:03:40 -0500

    this will change as well

--- PropBets.cs
+++ PropBets.cs
@@ -5,12 +5,21 @@ namespace nfl_picks_pool
 	public class PropBets
 	{
 		static List<int> CoachFiredWinners = new List<int>() { 9, 21, 25 };
-		
+        static List<int> CorrectRecordWinners = new List<int>() { 6, 11 };
+        static List<int> WorstRecord = new List<int>() { 17, 19, 21 };
 
-		public static int CalculatePropPoints(Player p)
+        public static int CalculatePropPoints(Player p)
 		{
 			int points = 0;
 			if (CoachFiredWinners.Contains(p.id))
+			{
+				points += 100;
+			}
+			if (CorrectRecordWinners.Contains(p.id))
+			{
+				points += 100;
+			}
+			if (WorstRecord.Contains(p.id))
 			{
 				points += 100;
 			}
@@ -22,14 +31,7 @@ namespace nfl_picks_pool
 			foreach (var playerEntry in playerTable)
 			{
 				Player p = playerEntry.Value;
-				if (CoachFiredWinners.Contains(p.id))
-				{
-					p.currentPlayerPoints = CalculatePropPoints(p) + p.bonusPlayerPoints;
-				}
-				else
-				{
-					p.currentPlayerPoints = p.bonusPlayerPoints;
-				}
+				p.currentPlayerPoints = CalculatePropPoints(p) + p.bonusPlayerPoints;
 			}
 		}
 	}
